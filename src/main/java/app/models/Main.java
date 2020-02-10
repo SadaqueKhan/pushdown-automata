0cfe399a81c2cbcbd208fc7f15319bf4328e004d
SadaqueKhan <sadaque.k@gmail.com>
2020-02-10 21:06:11 +0000

    Backend implementation

--- src/main/java/app/models/Main.java
+++ src/main/java/app/models/Main.java
@@ -1,12 +1,18 @@
 package app.models;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class Main {
 
+    // No. of vertices in graph
+    private static int v;
+
+    // adjacency list
+    private static ArrayList<Integer>[] adjList;
 
     public static void main(String[] args) throws IOException {
 
@@ -28,11 +34,20 @@ public class Main {
 
         TransitionModel transitionModel1 = new TransitionModel(stateModelA, "1", "", stateModelB, "X");
         stateModelA.attachTransitionToStateModel(transitionModel1);
-        TransitionModel transitionModel2 = new TransitionModel(stateModelB, "2", "X", stateModelC, "Y");
+        TransitionModel transitionModel2 = new TransitionModel(stateModelB, "2", "X", stateModelA, "Y");
         stateModelB.attachTransitionToStateModel(transitionModel2);
+        TransitionModel transitionModel3 = new TransitionModel(stateModelB, "", "Y", stateModelC, "Y");
+        stateModelB.attachTransitionToStateModel(transitionModel3);
+        TransitionModel transitionModel4 = new TransitionModel(stateModelA, "3", "Y", stateModelB, "X");
+        stateModelA.attachTransitionToStateModel(transitionModel4);
+        TransitionModel transitionModel5 = new TransitionModel(stateModelB, "", "X", stateModelC, "X");
+        stateModelB.attachTransitionToStateModel(transitionModel5);
 
         machineModel.addTransitionModelToTransitionModelSet(transitionModel1);
         machineModel.addTransitionModelToTransitionModelSet(transitionModel2);
+        machineModel.addTransitionModelToTransitionModelSet(transitionModel3);
+        machineModel.addTransitionModelToTransitionModelSet(transitionModel4);
+        machineModel.addTransitionModelToTransitionModelSet(transitionModel5);
 
         machineModel.setStartStateModel(machineModel.findStartStateModel());
 
@@ -71,7 +86,7 @@ public class Main {
                 if (checkAcceptance(numberOfSymbolsRead, splitUserInputArrayList.size(), transition.getResultingStateModel())) {
                     return String.valueOf(stringOfTransitions);
                 } else {
-                    System.out.println(String.valueOf(stringOfTransitions));
+
                     return anotherTransition(transition, stringOfTransitions, numberOfSymbolsRead, splitUserInputArrayList, stack);
                 }
             }
@@ -83,11 +98,8 @@ public class Main {
 
     public static String anotherTransition(TransitionModel computedTransition, StringBuilder stringOfTransitions, int numberOfSymbolsRead, List<String> splitUserInputArrayList, Stack<String> stack) {
 
-        System.out.println("Another Transition: ");
+        System.out.println("Another Transition: " + computedTransition);
         for (TransitionModel nextTransition : computedTransition.getResultingStateModel().getTransitionModelsAttachedToStateSet()) {
-
-            System.out.println(nextTransition.getStackSymbolToPop());
-            System.out.println(stack.peek());
             if ((nextTransition.getInputSymbol().equals(splitUserInputArrayList.get(numberOfSymbolsRead)) || nextTransition.getInputSymbol().equals("")) && nextTransition.getStackSymbolToPop().equals(stack.peek())) {
                 stringOfTransitions.append(nextTransition.toString()).append("\n");
                 ++numberOfSymbolsRead;
@@ -97,10 +109,10 @@ public class Main {
                     //then return string of transitions
                     return String.valueOf(stringOfTransitions);
                 } else {
+                    System.out.println("here");
                     anotherTransition(nextTransition, stringOfTransitions, numberOfSymbolsRead, splitUserInputArrayList, stack);
                 }
             }
-
 
         }
         return String.valueOf(stringOfTransitions);
@@ -114,6 +126,50 @@ public class Main {
     private static void updateStack(Stack<String> stack, String toPush) {
         stack.pop();
         stack.push(toPush);
+    }
+
+    private static void printAllPaths(int s, int d) {
+        boolean[] isVisited = new boolean[v];
+        ArrayList<Integer> pathList = new ArrayList<>();
+
+        //add source to path[]
+        pathList.add(s);
+
+        //Call recursive utility
+        printAllPathsUtil(s, d, isVisited, pathList);
+    }
+
+    private static void printAllPathsUtil(Integer u, Integer d,
+                                          boolean[] isVisited,
+                                          List<Integer> localPathList) {
+
+        // Mark the current node
+        isVisited[u] = true;
+
+        if (u.equals(d)) {
+            System.out.println(localPathList);
+            // if match found then no need to traverse more till depth
+            isVisited[u] = false;
+            return;
+        }
+
+        // Recur for all the vertices
+        // adjacent to current vertex
+        for (Integer i : adjList[u]) {
+            if (!isVisited[i]) {
+                // store current node
+                // in path[]
+                localPathList.add(i);
+                printAllPathsUtil(i, d, isVisited, localPathList);
+
+                // remove current node
+                // in path[]
+                localPathList.remove(i);
+            }
+        }
+
+        // Mark the current node
+        isVisited[u] = false;
     }
 }
 
