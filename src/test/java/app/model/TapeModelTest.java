7c486a7166d95826b833ab103da8e94cfdb9f9d8
SadaqueKhan <sadaque.k@gmail.com>
2020-03-25 18:42:14 +0000

    Finish test for tape model
    - Executable code

--- src/test/java/app/model/TapeModelTest.java
+++ src/test/java/app/model/TapeModelTest.java
@@ -1,3 +1,51 @@
 package app.model;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 public class TapeModelTest {
+    @Test
+    public void readingSymbolFromTheTapeShouldResultInTheHeadUpdating() {
+        TapeModel tapeModel = new TapeModel();
+        tapeModel.loadInput("test");
+        tapeModel.readSymbol();
+        assertEquals(1, tapeModel.getHead());
+    }
+    @Test
+    public void retrievingAInputSymbolFromTheTapeShouldResultInAInputSymbolToBeReturned() {
+        TapeModel tapeModel = new TapeModel();
+        tapeModel.loadInput("test");
+        assertEquals("t", tapeModel.getAtHead());
+        tapeModel.readSymbol();
+        assertEquals("e", tapeModel.getAtHead());
+        tapeModel.readSymbol();
+        assertEquals("s", tapeModel.getAtHead());
+        tapeModel.readSymbol();
+        assertEquals("t", tapeModel.getAtHead());
+        tapeModel.readSymbol();
+        assertEquals("ε", tapeModel.getAtHead());
+    }
+    @Test
+    public void checkAnEmptyTapeShouldReturnTrue() {
+        TapeModel tapeModel = new TapeModel();
+        assertEquals(0, tapeModel.tapeSize());
+        assertTrue(tapeModel.isEmpty());
+    }
+    @Test
+    public void settingHeadShouldReturnANewElementAtTheHead() {
+        TapeModel tapeModel = new TapeModel();
+        tapeModel.loadInput("test");
+        assertEquals("t", tapeModel.getAtHead());
+        tapeModel.setHead(2);
+        assertEquals("s", tapeModel.getAtHead());
+    }
+    @Test
+    public void loadingTapeContentWithTheInputWordShouldContainInputSymbolsInTheTape() {
+        String inputWord = "test";
+        TapeModel tapeModel = new TapeModel();
+        tapeModel.loadInput(inputWord);
+        for (String inputSymbol : tapeModel.getInputTape()) {
+            assertThat(inputWord, containsString(inputSymbol));
+        }
+    }
 }
